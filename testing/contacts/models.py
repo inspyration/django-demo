32e23c3f8969395809a811dc5814fed0eb6a5b80
Sébastien CHAZALLET <s.chazallet@gmail.com>
2014-11-12 11:17:59 +0100

    Contacts: add model

--- testing/contacts/models.py
+++ testing/contacts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Position(models.Model):
+    name = models.CharField(max_length=32)
+
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
+    position = models.ForeignKey(Position)
+    create_date = models.DateTimeField('date created')
