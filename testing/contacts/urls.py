2f4badfd4061ad15ae6f60df5f02950f831c6ed2
Sébastien CHAZALLET <s.chazallet@gmail.com>
2014-11-12 15:46:10 +0100

    Datatables integration

--- testing/contacts/urls.py
+++ testing/contacts/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = patterns(
     "",
     url(r"^$", views.index, name="index"),
+    url(r"^data/$", views.Data.as_view(), name="data"),
     url(r"^(?P<contact_id>\d+)/$", views.show, name="show"),
     url(r"^add/$", views.show, name="show"),
     url(r"^(?P<contact_id>\d+)/edit/$", views.edit, name="edit"),
--- testing/contacts/views.py
+++ testing/contacts/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django_datatables_view.base_datatable_view import BaseDatatableView
+
+from django.db.models import Q
 
 from .models import Contact
 
@@ -20,3 +23,42 @@ def show(request, contact_id=None):
 
 def edit(request, contact_id):
     return HttpResponse("Contacts edit page.")
+
+
+class Data(BaseDatatableView):
+    # The model we're going to show
+    model = Contact
+
+    # define the columns that will be returned
+    columns = ['first_name', 'last_name', 'position', 'create_date']
+
+    # define column names that will be used in sorting
+    # order is important and should be same as order of columns
+    # displayed by datatables. For non sortable columns use empty
+    # value like ''
+    order_columns = ['first_name', 'last_name', 'position', 'create_date']
+
+    # set max limit of records returned, this is used to protect our site if someone tries to attack our site
+    # and make it return huge amount of data
+    max_display_length = 100
+
+    def render_column(self, row, column):
+        # We want to render user as a custom column
+        if column == 'position':
+            return row.position.name
+        elif column == 'create_date':
+            return row.create_date.strftime("%a %d %b %Y, à %H:%M:%S"),
+        else:
+            return super(Data, self).render_column(row, column)
+
+    def filter_queryset(self, qs):
+        search = self.request.GET.get('search[value]', None)
+
+        if search:
+            qs = qs.filter(
+                Q(first_name__contains=search) |
+                Q(last_name__contains=search) |
+                Q(position__name__contains=search)
+            )
+
+        return qs
