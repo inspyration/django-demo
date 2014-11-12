c5910ee89a4d1310b1032c7a8dfb960a136e114c
Sébastien CHAZALLET <s.chazallet@gmail.com>
2014-11-12 12:21:04 +0100

    App contacts: first index page is working

--- testing/contacts/views.py
+++ testing/contacts/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Contact
+
 
 def index(request):
-    return HttpResponse("Contacts index page.")
+    return render(
+        request,
+        'index.html',
+        {
+            "contacts": Contact.objects.all().order_by("-create_date")
+        },
+    )
 
 
 def show(request, contact_id):
