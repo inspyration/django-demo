2d4f14dff2224f5d147685ba26b6d691069b9b1f
Sébastien CHAZALLET <s.chazallet@gmail.com>
2014-11-12 17:54:44 +0100

    App contacts: first form

--- testing/contacts/views.py
+++ testing/contacts/views.py
@@ -1,17 +1,19 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
 from django_datatables_view.base_datatable_view import BaseDatatableView
 from django.views import generic
 
 from django.db.models import Q
 
 from .models import Contact
+from .forms import ContactForm
 
 
 def index(request):
     return render(
         request,
-        'index.html',
+        "index.html",
         {
             "contacts": Contact.objects.all().order_by("-create_date")
         },
@@ -20,25 +22,42 @@ def index(request):
 
 class Show(generic.DetailView):
     model = Contact
-    template_name = 'show.html'
+    template_name = "show.html"
 
 
 def edit(request, contact_id):
-    return HttpResponse("Contacts edit page.")
+    contact = get_object_or_404(Contact, pk=contact_id)
+
+    if request.method == "POST":
+        form = ContactForm(request.POST, instance=contact)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse("show", args=(contact.id,)))
+    else:
+        form = ContactForm(instance=contact)
+
+    return render(
+        request,
+        "edit.html",
+        {
+            "contact": contact,
+            "form": form,
+        },
+    )
 
 
 class Data(BaseDatatableView):
-    # The model we're going to show
+    # The model we"re going to show
     model = Contact
 
     # define the columns that will be returned
-    columns = ['first_name', 'last_name', 'position', 'create_date']
+    columns = ["first_name", "last_name", "position", "create_date"]
 
     # define column names that will be used in sorting
     # order is important and should be same as order of columns
     # displayed by datatables. For non sortable columns use empty
-    # value like ''
-    order_columns = ['first_name', 'last_name', 'position', 'create_date']
+    # value like ""
+    order_columns = ["first_name", "last_name", "position", "create_date"]
 
     # set max limit of records returned, this is used to protect our site if someone tries to attack our site
     # and make it return huge amount of data
@@ -46,15 +65,15 @@ class Data(BaseDatatableView):
 
     def render_column(self, row, column):
         # We want to render user as a custom column
-        if column == 'position':
+        if column == "position":
             return row.position.name
-        elif column == 'create_date':
+        elif column == "create_date":
             return row.create_date.strftime("%a %d %b %Y, à %H:%M:%S"),
         else:
             return super(Data, self).render_column(row, column)
 
     def filter_queryset(self, qs):
-        search = self.request.GET.get('search[value]', None)
+        search = self.request.GET.get("search[value]", None)
 
         if search:
             qs = qs.filter(
