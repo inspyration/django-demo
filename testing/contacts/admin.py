40481ad36a223836c31054deec19cd7f930f038c
Sébastien CHAZALLET <s.chazallet@gmail.com>
2015-04-16 11:41:20 +0200

    Do over

--- testing/contacts/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Position, Contact
-
-admin.site.register(Position)
-admin.site.register(Contact)
--- testing/testing/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
-
-
-urlpatterns = patterns(
-    "",
-    url(r"^contacts/", include("contacts.urls")),
-    url(r"^admin/", include(admin.site.urls)),
-)
